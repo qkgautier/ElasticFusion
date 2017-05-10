5b16b56ea56df64fd9845cbee2ac7d3395ebc38d
Quentin Gautier <qkgautier@eng.ucsd.edu>
2017-05-10 00:12:45 -0700

    Fixed RealSense and CMakeLists. Modified build script to add 16.04.

--- build.sh
+++ build.sh
@@ -10,17 +10,24 @@ if [[ $version == *"14.04"* ]] ; then
     sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
     rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
     sudo apt-get update
+    sudo apt-get install -y openjdk-7-jdk cuda-7-5
 elif [[ $version == *"15.04"* ]] ; then
     wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1504/x86_64/cuda-repo-ubuntu1504_7.5-18_amd64.deb
     sudo dpkg -i cuda-repo-ubuntu1504_7.5-18_amd64.deb
     rm cuda-repo-ubuntu1504_7.5-18_amd64.deb
     sudo apt-get update
+    sudo apt-get install -y openjdk-7-jdk cuda-7-5
+elif [[ $version == *"16.04"* ]] ; then
+    echo "Make sure you have installed CUDA manually."
+    echo "Press ENTER to continue. Press CTRL-C to Cancel."
+    read
+    sudo apt-get install -y libopenni2-dev
 else
-    echo "Don't use this on anything except 14.04 or 15.04"
+    echo "Don't use this on anything except 14.04 or 15.04 or 16.04"
     exit
 fi
 
-sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev libglew-dev cuda-7-5 libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
+sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev freeglut3-dev libglew-dev libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
 
 #Installing Pangolin
 git clone https://github.com/stevenlovegrove/Pangolin.git
@@ -31,11 +38,13 @@ cmake ../ -DAVFORMAT_INCLUDE_DIR="" -DCPP11_NO_BOOST=ON
 make -j8
 cd ../..
 
-#Up to date OpenNI2
-git clone https://github.com/occipital/OpenNI2.git
-cd OpenNI2
-make -j8
-cd ..
+if [[ $version == *"14.04"* ]] || [[ $version == *"15.04"* ]] ; then
+    #Up to date OpenNI2
+    git clone https://github.com/occipital/OpenNI2.git
+    cd OpenNI2
+    make -j8
+    cd ..
+fi
 
 #Actually build ElasticFusion
 cd ../Core
